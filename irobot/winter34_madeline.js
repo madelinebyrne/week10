3b97bdc5dbb6e6e31d39eec968a545b3abeffc8e
Madeline Byrne <madelinebyrne@seattleacademy.org>
2016-02-26 13:18:11 -0800

    add controls for robot

--- irobot/winter34_madeline.js
+++ irobot/winter34_madeline.js
@@ -15,6 +15,80 @@ var port = (process.argv[2] ? Number(process.argv[2]) : 1025);
 app.listen(port);
 console.log("listening on port ", port);
 
+// listen for the "keypress" event
+process.stdin.on('keypress', function(ch, key) {
+    console.log('got "keypress"', key);
+    if (!key) return;
+
+    if (key.name == 'w') {
+        console.log("moved up");
+        //up(10);
+        velocity.left += 10;
+        velocity.right += 10;
+        robot.drive(velocity);
+        //robot.rtsTrue();
+    } else if (key.name == 's') {
+        velocity.left -= 10;
+        velocity.right -= 10;
+        robot.drive(velocity);
+        //robot.rtsFalse();
+    } else if (key.name == 'd') {
+        velocity.left -= 5;
+        velocity.right += 5;
+        robot.drive(velocity);
+        console.log("moved right");
+        //turnRightDegrees(10);
+    } else if (key.name == 'a') {
+        velocity.left += 5;
+        velocity.right -= 5;
+        robot.drive(velocity);
+        console.log("moved left");
+        //turnLeftDegrees(10);
+    } else if (key.name == 'space') {
+        console.log("stop me");
+        velocity.left = 0;
+        velocity.right = 0;
+        robot.drive(velocity);
+    } else if (key.name == 'f') {
+        console.log("fullMode");
+        robot.fullMode();
+    } else if (key.name == 'p') {
+        console.log("passiveMode");
+        robot.passiveMode();
+    } else if (key.name == 'g') {
+        console.log("go -- safeMode");
+        robot.safeMode();
+    } else if (key.name == 'q') {
+        console.log("play song");
+        mysong = [
+            [640, 100],
+            [650, 100],
+	[660,100],
+[670,100],
+[680,100],
+[690,100],
+[700,100],
+[710,100],
+[720,100],
+[730,100],
+[740,100],
+[750,100],
+[760,100],
+[770,100]
+        ];
+        robot.sing(mysong);
+    }
+
+
+    if (key && key.ctrl && key.name == 'c') {
+        console.log('shift control.c');
+        process.exit();
+        //process.stdin.pause();
+    }
+});
+
+
+
 function handler(req, res) {
     fs.readFile(__dirname + '/' + pageName, processFile);
 
