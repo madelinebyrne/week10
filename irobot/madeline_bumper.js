3d36dec6c0064a75fe28f3b09af11718f96a39a4
madelinebyrne <Madelinebyrne@seattleacademy.org>
2016-03-01 12:47:40 -0800

    add backing up

--- irobot/madeline_bumper.js
+++ irobot/madeline_bumper.js
@@ -6,4 +6,13 @@ robot.on('ready', function () {
   console.log('READY');
 });
  mysong = [[640, 200]];
-robot.on('bump', function (e) { console.log('BUMP', e); robot.sing(mysong); });
+robot.on('bump', function (e) 
+{ 
+  console.log('BUMP', e); 
+  robot.sing(mysong); 
+  setTimeout(function() 
+  {
+    socket.emit('drive', {left: -200, right: -200});
+  }, 1000)
+  
+});
