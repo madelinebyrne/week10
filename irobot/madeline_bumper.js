20b494200c09279ffe4b6e8dcf1ed75b72218023
Madeline Byrne <madelinebyrne@seattleacademy.org>
2016-03-01 13:20:18 -0800

    bumper code throwing syntax error

--- irobot/madeline_bumper.js
+++ irobot/madeline_bumper.js
@@ -2,8 +2,11 @@ var irobot = require('./index');
 
 var robot = new irobot.Robot('/dev/ttyUSB0');
 
-robot.on('ready', function () {
+robot.on('ready', function () 
+{
   console.log('READY');
-});
- mysong = [[640, 200]];
-robot.on('bump', function (e) { console.log('BUMP', e); robot.sing(mysong); setTimeout(function() {socket.emit('drive', {left: -200, right: -200}, 1000)});
+})
+
+mysong = [[640, 200]];
+
+robot.on('bump', function (e) { console.log('BUMP', e); robot.sing(mysong); setTimeout(function() {socket.emit('drive', {left: -200, right: -200}, 1000)})
