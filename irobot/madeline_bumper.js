57c0039bfb74d82bd6e5522d89b7dcabe1642fa4
madelinebyrne <Madelinebyrne@seattleacademy.org>
2016-03-01 12:39:45 -0800

    take mysong declaration out of function

--- irobot/madeline_bumper.js
+++ irobot/madeline_bumper.js
@@ -5,6 +5,5 @@ var robot = new irobot.Robot('/dev/ttyUSB0');
 robot.on('ready', function () {
   console.log('READY');
 });
-
-robot.on('bump', function (e) { console.log('BUMP', e); mysong = [
-            [640, 200]]; robot.sing(mysong); });
+ mysong = [[640, 200]];
+robot.on('bump', function (e) { console.log('BUMP', e); robot.sing(mysong); });
