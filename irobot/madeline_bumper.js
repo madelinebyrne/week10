62c5cc38fc0a184fe767e6fa8960ff0012092a2d
madelinebyrne <Madelinebyrne@seattleacademy.org>
2016-03-01 12:34:44 -0800

    added song to test bumper

--- irobot/madeline_bumper.js
+++ irobot/madeline_bumper.js
@@ -6,4 +6,5 @@ robot.on('ready', function () {
   console.log('READY');
 });
 
-robot.on('bump', function (e) { console.log('BUMP', e); });
+robot.on('bump', function (e) { console.log('BUMP', e); mysong = [
+            [640, 200]; robot.sing(mysong); });
